7546496e1a489001091d1755655edfde4a8a1c5a
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-09-13 07:14:06 -0500

    adding the random numbers

--- dices game.js
+++ dices game.js
@@ -6,15 +6,14 @@ function playGame() { // master function
 
     // Player 1's turn
     let rollArray = roll();
-    Score(rollArray);
     
     // console.log('total', total);
 }
 
 function roll() {
     let roll1 = Math.floor(Math.random()*4) +1;
-    let roll2 = Math.floor(Math.random()*6)+1;
-    let roll3 = Math.floor(Math.random()*8)+1;
+    let roll2 = Math.floor(Math.random()*6) +1;
+    let roll3 = Math.floor(Math.random()*8) +1;
     let roll4 = Math.floor(Math.random()*10)+1;
     let roll5 = Math.floor(Math.random()*12)+1;
     let roll6 = Math.floor(Math.random()*20)+1;
@@ -26,20 +25,17 @@ function roll() {
     console.log(roll5);
     console.log(roll6);
 
-    return 
+
+let subScore = [roll1,roll2,roll3,roll4,roll5,roll6];
+let playerOneScore = roll1+roll2+roll3+roll4+roll5+roll6;
+ 
+
+
+   console.log ('Player 1 =', playerOneScore)
 }
-    
 
-
-function Score(roll1,roll2,roll3,roll4,roll5,roll6){
-    // consider passing in an array of the rolls
-    // write a loop to go over the array and total up the rolls
-    let total = 0;
-
-  let score = (roll1+roll2+roll3+roll4+roll5+roll6);
-  return score;
- }
-
+   //odd and even coding
+  
 // let playerOnescore=
  
 playGame();

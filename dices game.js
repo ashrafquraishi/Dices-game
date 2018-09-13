e9b549b9830686885cadaaf3326551335eb8c44d
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-09-13 14:06:46 -0500

    player 2 random numbers

--- dices game.js
+++ dices game.js
@@ -10,9 +10,9 @@ function playGame() { // master function
     let rollArray = roll();
     
     // console.log('total', total);
-}
+ }
 
-function roll() {
+ function roll() {
     let roll1 = Math.floor(Math.random()*4) +1;
     let roll2 = Math.floor(Math.random()*6) +1;
     let roll3 = Math.floor(Math.random()*8) +1;
@@ -28,33 +28,33 @@ function roll() {
     console.log(roll6);
 
 
-    let subScore = [roll1,roll2,roll3,roll4,roll5,roll6];
-    let Player1Score = roll1+roll2+roll3+roll4+roll5+roll6;
-    console.log ('Player 1 Score=', Player1Score)
+     let subScore = [roll1,roll2,roll3,roll4,roll5,roll6];
+     let Player1Score = roll1+roll2+roll3+roll4+roll5+roll6;
+     console.log ('Player 1 Score=', Player1Score)
     
-    let isEven;
-    if (Player1Score%2 === 0){
+     let isEven;
+     if (Player1Score%2 === 0){
       isEven = true;
-    }
-    else if (Player1Score%2 !== 0){
+     }
+     else if (Player1Score%2 !== 0){
       isEven = false
-    }
-    let userInput= prompt("player 2: Enter odd or even")
+      }
+     let userInput= prompt("player 2: Enter odd or even")
 
-    if(isEven){
+     if(isEven){
         if (userInput === "even"){
             console.log ('your guess was right')
         }
         else{
-            //wrong
+            console.log ("your guess was wrong")
         }
-    }
-    else{
+     }
+     else{
         if (userInput === "odd"){
             console.log ('your guess was right')
         }
         else{
-            //wrong
+            console.log ("your guess was wrong")
         }
     }
 }
